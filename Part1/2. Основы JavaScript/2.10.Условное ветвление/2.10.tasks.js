35c1f950aac6c1d5fc0faeef6561a0f3e0e254e2
Pasha <mikatoXD@gmail.com>
2023-05-05 17:54:50 +0300

    1

--- Part1/2. Основы JavaScript/2.10.Условное ветвление/2.10.tasks.js
+++ Part1/2. Основы JavaScript/2.10.Условное ветвление/2.10.tasks.js
@@ -1,7 +1,12 @@
+
 "use strict";
 let mainArr = [
     function (){
-
+        let result;
+        let a = 1;
+        let b = 2;
+        result = (a + b < 4) ? "Мало" : "Много";
+        console.log(result);
     },
     function (){
 
