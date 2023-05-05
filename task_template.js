8c8ebc4ff5bb24cb587cb3a63cf04ca087a13c85
Pasha <mikatoXD@gmail.com>
2023-05-05 16:22:07 +0300

    1

--- task_template.js
+++ task_template.js
@@ -1,21 +1,44 @@
+// "use strict";
+// let mainArr = [function task1(){
+//
+// },
+// function task2(){
+//
+// },
+// function task3(){
+//
+// },
+// function task4(){
+//
+// },
+// function task5(){
+//
+// }]
+//
+// for (let i=0; i<mainArr.length; i++){
+//     console.log('task' + i);
+//     mainArr[i]();
+// }
+
 "use strict";
-let mainArr = [function task1(){
+let mainArr = [function (){
 
-},
-function task2(){
+    },
+    function (){
 
-},
-function task3(){
+    },
+    function (){
 
-},
-function task4(){
+    },
+    function (){
 
-},
-function task5(){
+    },
+    function (){
 
-}]
+    }]
 
 for (let i=0; i<mainArr.length; i++){
     console.log('task' + i);
     mainArr[i]();
+    console.log();
 }
